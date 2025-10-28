537e8abb244509e6d39c874ec8667b696e0b61bf
Ville-Pekka Koskinen <villepekka.koskinen@gmail.com>
2025-10-28 16:32:26 +0200

    Added lighting

--- main.js
+++ main.js
@@ -8,13 +8,14 @@ renderer.outputcolorSpace = THREE.SRGBColorSpace;
 renderer.setSize(window.innerWidth*0.9, window.innerHeight*0.9);
 renderer.setClearColor(0x000000);
 renderer.setPixelRatio(window.devicePixelRatio);
+renderer.shadowMap.enabled = true;
 
 document.body.appendChild(renderer.domElement);
 
 const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 100);
-camera.position.set(0, 5, 10);
+camera.position.set(-10, 5, 0);
 camera.lookAt(0, 0, 0);
 
 // Add Ground
@@ -24,15 +25,42 @@ const groundMaterial = new THREE.MeshStandardMaterial({ color: 0x808080, side: T
 const groundMesh = new THREE.Mesh(groundGeo, groundMaterial);
 scene.add(groundMesh);
 
-// Add Light
-const ambientLight = new THREE.AmbientLight(0xffffff, 1.0);
+// Add Lights
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.05);
 scene.add(ambientLight);
+
+const lightvalues = [[new THREE.Vector3(10.269,10.269,10.269), 0xE3E6FF, 300.0, true],
+					[new THREE.Vector3(-14.3872,6.14344,10.104), 0xFFF9DD, 150.0, false],
+					[new THREE.Vector3(-0.573415,10.4088,-15.4321), 0xE3E6FF, 50.0, false]
+				];
+//const lightvalues = [[new THREE.Vector3(10.269,10.269,10.269), 0xE3E6FF]];
+
+for (const [lposition, lcolor, lintensity, lcastshadowbool] of lightvalues) {
+	const spotlight = new THREE.SpotLight(lcolor, lintensity, 0.0, Math.PI / 10.0);
+	spotlight.position.copy(lposition);
+	if (lcastshadowbool == true) {
+		spotlight.castShadow = true;
+	}
+	spotlight.shadow.camera.near = 0.1;
+	spotlight.shadow.camera.far = 25.0;
+	scene.add(spotlight);
+	// helpers
+	//scene.add( new THREE.CameraHelper( spotlight.shadow.camera ) );
+}
+
 
 // Add Banana
 const loader = new GLTFLoader();
 loader.setPath("/banaani/");
 loader.load("banaani.gltf", (gltf) => {
 	const banana = gltf.scene;
+	gltf.scene.traverse(function (node) {
+
+                    if (node.isMesh || node.isLight) node.castShadow = true;
+
+                    if (node.isMesh || node.isLight) node.receiveShadow = true;
+
+                });
 	//banana.scale.set(0.5, 0.5, 0.5);
 	banana.position.set(0, 0, 0);
 	scene.add(banana);
